b9be4f2b59d54f9af89b2457c2229aa08ab70a00
Max Murphy <maxwell.murphy@ucsf.edu>
2016-05-15 20:48:12 -0700

    Namespacing
    static folder is now namespaced

--- app/events.py
+++ app/events.py
@@ -1,13 +1,19 @@
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, send_from_directory, redirect
+from werkzeug.exceptions import NotFound
+from flask.helpers import safe_join
 
-plasmotrack = Blueprint('plasmotrack', import_name=__name__, static_folder='static')
+plasmotrack = Blueprint('plasmotrack', import_name=__name__, static_folder='static/plasmotrack-js')
 
 
 @plasmotrack.route('/<path:path>')
 def catch_all(path):
-    return send_from_directory('static/plasmotrack-js', path)
+    try:
+        return send_from_directory(plasmotrack.static_folder, path)
+    except NotFound:
+        return redirect("/")
 
 
 @plasmotrack.route('/')
 def index():
-    return plasmotrack.send_static_file('plasmotrack-js/index.html')
+    return plasmotrack.send_static_file('index.html')
+
