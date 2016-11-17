70a2f4f76ee6fa066fd97e6dac0c8278cfbe5df6
Max Murphy <maxwell.murphy@ucsf.edu>
2016-11-16 16:19:57 -0800

    Add License

--- app/static/plasmotrack-js/app/plasmomapper/api.ts
+++ app/static/plasmotrack-js/app/plasmomapper/api.ts
@@ -1,2 +1,18 @@
+// MicroSPAT is a collection of tools for the analysis of Capillary Electrophoresis Data
+// Copyright (C) 2016  Maxwell Murphy
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 export var SERVER_BASE = '';
 export var API_BASE = SERVER_BASE + '/microspat/api/v1';
